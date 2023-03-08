ab92e7cc676b6bef3a33d26fa59758f684dfda9c
ivayloyordanoff <112658912+ivayloyordanoff@users.noreply.github.com>
2023-03-08 16:05:49 +0200

    Exercises

---
+++ static_and_class_methods_lab/shop.py
@@ -0,0 +1,38 @@
+from typing import Dict
+
+
+class Shop:
+    def __init__(self, name: str, type_shop: str, capacity: int):
+        self.name = name
+        self.type = type_shop
+        self.capacity = capacity
+        self.items: Dict[str: int] = {}
+
+    @classmethod
+    def small_shop(cls, name: str, type_shop: str):
+        return cls(name, type_shop, 10)
+
+    def add_item(self, item_name: str):
+        if self.capacity > sum(self.items.values()):
+            if item_name not in self.items:
+                self.items[item_name] = 0
+
+            self.items[item_name] += 1
+
+            return f"{item_name} added to the shop"
+
+        return "Not enough capacity in the shop"
+
+    def remove_item(self, item_name: str, amount: int):
+        if item_name not in self.items or self.items[item_name] < amount:
+            return f"Cannot remove {amount} {item_name}"
+
+        self.items[item_name] -= amount
+
+        if self.items[item_name] == 0:
+            del self.items[item_name]
+
+        return f"{amount} {item_name} removed from the shop"
+
+    def __repr__(self):
+        return f"{self.name} of type {self.type} with capacity {self.capacity}"
